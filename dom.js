77f354375cc472c31d787eb8239aefd818b90433
amanavinash <avinashverma13311@gmail.com>
2022-12-11 12:37:55 +0530

    add dom.js to html

---
+++ dom.js
@@ -0,0 +1,16 @@
+
+//var HeadTitle =document.getElementById("main-header");
+//console.log();
+//console.log(document.getElementById("main-header"));
+var headerTitle =document.getElementById("main-header");
+//console.log( headerTitle);
+//headerTitle.textContent='hello' ;
+headerTitle.innerHTML='<h1>hello</h1>' ;
+headerTitle.style.color='green';
+headerTitle.style.borderBlock='solid 3px #000';
+
+//HeadTitle.textContant='hello' ;
+
+
+
+
