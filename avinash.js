0994292d0cbbfd07cdd38018cbfdfc03b48b86e8
amanavinash <avinashverma13311@gmail.com>
2022-12-20 12:51:16 +0530

    task carring

--- avinash.js
+++ avinash.js
@@ -1,99 +1,57 @@
-
-//var HeadTitle =document.getElementById("main-header");
-//console.log();
-//console.log(document.getElementById("main-header"));
-//var headerTitle =document.getElementById("main-header");
-//console.log( headerTitle);
-//headerTitle.textContent='hello' ;
-//headerTitle.innerHTML='<h1>hello</h1>' ;
-//headerTitle.style.color='green';
-//headerTitle.style.borderBlock='solid 3px #000';
-//HeadTitle.textContant='hello' ;
-// var items =document.getElementsByClassName("list-group-item");
-// console.log(items);
-// console.log(items[1]);
-// items[1].textContent="hello" ;
-// for(var i=0;i<items.length;i++)
+// let multiply = function (x,y)
 // {
-// items[i].style.fontWeight='bold' ;
+//  console.log(x*y)   ;
 // }
-// items[1].style.backgroundColor='green' ;
-// var li =document.getElementsByTagName('li');
-// console.log(li);
-// console.log(li[1]);
-// li[1].textContent="hello" ;
-// for(var i=0;i<li.length;i++)
-// {
-// li[i].style.fontWeight='bold' ;
-
- //li[1].style.backgroundColor='green' ;
-//  var header=document.querySelector('#main-header');
-//  var item=document.querySelector('.list-group-item');
-//  item.style.color='red';
-//  var secondItem=document.querySelector('.list-group-item:nth-child(2)');
-//  secondItem.style.color='green';
-//  var odd=document.querySelectorAll('.list-group-item:nth-child(odd)');
-//  for(var i=0;i<odd.length;i++)
-//  {
-//  odd[i].style.backgroundColor= '#ccc' ;
- 
-//  }
- //  //li[1].style.backgroundColor='green' ;
- // var header=document.querySelector('#main-header');
- // var item=document.querySelector('.list-group-item');
- // item.style.color='red';
- // var secondItem=document.querySelector('.list-group-item:nth-child(2)');
- // secondItem.style.color='green';
- // var odd=document.querySelectorAll('.list-group-item:nth-child(odd)');
- // for(var i=0;i<odd.length;i++)
- // {
- // odd[i].style.backgroundColor= '#ccc' ;
- 
- // }
- //paren t
-  //var itemList=document.querySelector('#items');
- // console.log(itemList.parentNode);
- // itemList.parentNode.style.backgroundColor='#f4f4f4' ;
- // console.log(itemList.parentNode.parentNode.parentNode);
- 
- 
-  var itemList=document.querySelector('#items');
- // console.log(itemList.parentElement);
- // itemList.parentElement.style.backgroundColor='#f4f4f4' ;
- // console.log(itemList.parentElement.parentElement.parentElement);
- // console.log(itemList.childNodes);
- // console.log(itemList.children);
- // console.log(itemList.children[1]);
- // console.log(itemList.firstElementChild);
- // itemList.firstElementChild.textContent='hello';
- 
- 
- //  console.log(itemList.lastElementChild);
- //  itemList.lastElementChild.textContent='hello 2';
- // console.log(itemList.nextSibling);
- 
- // console.log(itemList.nextElementSibling);
- // console.log(itemList.previousElementSibling);
- 
- 
- 
- var newDiv=document.createElement('div');
-
- newDiv.className='hello';
- newDiv.id='hello 1'
- 
- newDiv.setAttribute('title','Hello div') ;
 
 
- var newDivText=document.createTextNode('hello world');
- newDiv.appendChild(newDivText);
+let multiply = function (x)
+{
+return function(y)
+{
+console.log(x*y) ;
+}   
+}
 
- var container =document.getElementById('aman');
- var h1=document.querySelector('header h1') ;
- console.log(newDiv);
- console.log(container);
-  container.insertBefore( newDiv, h1);
- 
- 
- 
- 
+let multiplyBYTwo=multiply( 2 ) ;
+multiplyBYTwo(5) ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let multiplyBYTwo=multiply.bind( this,2 ,5) 
+// // let multiplyBYTwo=multiply.bind( this,2) ; 
+// multiplyBYTwo(5) ;
+// multiplyBYTwo(15) ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
