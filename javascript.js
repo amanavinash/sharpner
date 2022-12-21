73749c96ee31e2c07683f7956fe566707d4d9182
amanavinash <avinashverma13311@gmail.com>
2022-12-21 12:14:51 +0530

    desine task 2

--- javascript.js
+++ javascript.js
@@ -1,48 +1,27 @@
 class student{
-    static count=0;  //static variable to call
-   constructor(name,age,ph_no,marks){
+  
+   constructor(name,age,marks){
    this.name=name;
    this.age=age;
-   this.ph_no=ph_no;
   this.marks=marks;
-  student.countStudent();
+ setPlaceMentAge(minPlaceMentAge) {
+console.log(this);
+return(minMarks)=>{
+console.log('inside eligiblityForCurrent company',this);
+if(this.marks >minMarks && this.age>minPlaceMentAge){
+  console.log(this.name + "reday for placement");
+  } else{
+    console.log(this.name + " not reday for placement");
   }
-  static countStudent(){
-     return(student.count++);     //this is how u access static variable
+}
+ }
+}
+} 
+  const Riya=new student('Riya',18,34);
   
-   }
-  eligible(){
-   if(this.marks >=40){
-  console.log(`${this.name} age ${age} is eligible`);
-  }
-  else if(this.marks<40){
-  console.log(`${this.name} age ${age} is not eligible`);
-   }
-  }
+  const Hiya=new student('Hiya',15,35);
+  Riya.setPlaceMentAge(18)(40);
   
-  }
-  
-  const Riya=new student('Riya',18,1234,34);
-  
-  const Hiya=new student('Hiya',15,2345,35);
-  
-  const Diya=new student('Diya',16,4567,60);
-  
-  const Siya=new student('Siya',17,7891,70);
-  
-  const Rooh=new student('Rooh',19,3456,80);
-  
-  console.log(student.countStudent());
-  
-  Riya.eligible();
-  
-  Hiya.eligible();
-  
-  Diya.eligible();
-  
-  Siya.eligible();
-  
-  Rooh.eligible();
   
   
   
