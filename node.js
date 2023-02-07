e2fa8b5b70766eb501022cb5996c3d0d4ac31859
amanavinash <avinashverma13311@gmail.com>
2023-02-07 14:10:09 +0530

    task

--- node.js
+++ node.js
@@ -1,8 +1,21 @@
- const http = require('http');
+const http = require('http');
 const server = http.createServer((req, res) => {
+const url=req.url;
+if(url==='/home'){
 
-res.setHeader('Content-Type', 'text/plain');
-res.write('avinash');
+  res.write('<html>');
+  res.write('<head><title>My first page</title></head>');
+  res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="summit">send</button></form><body>');
+  res.write('</html>');
+return res.end();
+}
+
+
+res.setHeader('Content-Type','text/html');
+res.write('<html>');
+res.write('<head><title>My first page</title></head>');
+res.write('<body><h1> Welcome to my Node Js project</h><body>');
+res.write('</html>');
 
  res.end();
  });
