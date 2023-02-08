2f732817cffa67ec206d27923d8253ab08395bed
amanavinash <avinashverma13311@gmail.com>
2023-02-08 16:53:24 +0530

    gjkhg

--- start.js
+++ start.js
@@ -5,24 +5,20 @@ const url=req.url;
  const method=req.method;
  const body = [];
 if(url==='/'){
-
   fs.readFile('message.text','utf8', (err, data) => {
+    if(err){
+      console.log(err);
+    }
     console.log(data);
  })
-
-
-
-
   res.write('<html>');
   res.write('<head><title>My first page</title></head>');
+  req.write(`<body>${data}</body>`)
   res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="summit">send</button></form><body>');
   res.write('</html>');
 return res.end();
-} 
-
- if(url==='/message'&&method=='POST')
+} else if(url==='/message'&& method=='POST')
  {
-
 req.on('data',(chunk)=>{
    console.log(chunk);
  body.push(chunk);
@@ -30,13 +26,12 @@ req.on('data',(chunk)=>{
  req.on('end',()=>{
  const parsedBody=Buffer.concat(body).toString();
  console.log(parsedBody);
- const message =parsedBody.split('=')[1] ;
  fs.writeFileSync('message.text',message);
 }) ;
  res.statusCode=302;
  res.setHeader('Location','/');
  return res.end();
- }
+ }else
 res.setHeader('Content-Type','text/html');
 res.write('<html>');
 res.write('<head><title>My first page</title></head>');
@@ -45,8 +40,8 @@ res.write('</html>');
 
  res.end();
  });
-
-server.listen(4000);
+// module.exports=requestHandler;
+ server.listen(4000);
 
 
 
