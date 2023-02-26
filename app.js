547f72907dad1957785c412307ceef582c2406cc
amanavinash <avinashverma13311@gmail.com>
2023-02-26 15:24:56 +0530

    task add product

--- app.js
+++ app.js
@@ -6,10 +6,8 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 
 const app = express();
-
 app.set('view engine', 'ejs');
 app.set('views', 'views');
-
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
@@ -20,5 +18,4 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
-
 app.listen(3000);
