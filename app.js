42c9f4491f9771f6723730ccef0292acec8aed8a
amanavinash <avinashverma13311@gmail.com>
2023-02-26 15:33:16 +0530

    taslk add product

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 
 const app = express();
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
@@ -18,4 +20,5 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
+
 app.listen(3000);
