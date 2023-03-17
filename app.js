77abed4dbfd750b98576bf2832d5c9a5697a4825
amanavinash <avinashverma13311@gmail.com>
2023-03-17 12:53:56 +0530

    task one to one

--- app.js
+++ app.js
@@ -1,30 +1,58 @@
 const path = require('path');
+
 const express = require('express');
-var cors = require('cors')
 const bodyParser = require('body-parser');
+
 const errorController = require('./controllers/error');
-const sequelize=require('./util/database')
+const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const app = express();
-app.use(cors()) ;
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const userRoutes = require('./routes/user');
-const expenseRoutes = require('./routes/expense');
-app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findById(1)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-app.use('/user',userRoutes);
-app.use('/expense',expenseRoutes);
+
 app.use(errorController.get404);
+
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+
 sequelize
+  // .sync({ force: true })
   .sync()
   .then(result => {
- 
-   app.listen(3000);
+    return User.findById(1);
+    // console.log(result);
   })
- .catch(err => console.log(err));
-
+  .then(user => {
+    if (!user) {
+      return User.create({ name: 'Max', email: 'test@test.com' });
+    }
+    return user;
+  })
+  .then(user => {
+    // console.log(user);
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
