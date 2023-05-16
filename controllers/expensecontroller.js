2ebaca165129d7f05b465a4c9ccc58d1d3c03be7
amanavinash <avinashverma13311@gmail.com>
2023-05-16 17:50:15 +0530

    task Making Expense App Production ready

--- controllers/expensecontroller.js
+++ controllers/expensecontroller.js
@@ -1,15 +1,19 @@
 const Expense = require('../models/expense');
+const Expense = require('../models/expense');
 exports.addUser = async(req, res, next) => {
 try{
 const amount=req.body.amount;
 const description=req.body.description;
 const category = req.body.category;
-const Users=await Expense.create({amount:amount,description:description,category:category,userId:req.user.id}) 
+const Users=await Expense.create({amount:amount,description:description,category:category,userId:req.user.id},{transaction: t}) 
 .then((User) => {
     const totalExpenses = Number(req.user.totalExpenses) + Number(amount);
     User.update(
       { totalExpenses: totalExpenses },
-      { where: { id: req.user.id } }
+      { where: { id: req.user.id },
+      transaction: t
+    
+    }
     );
   });
   res.status(200).json({ newUserDetails: Users });
@@ -19,14 +23,6 @@ const Users=await Expense.create({amount:amount,description:description,category
 }
 };
 
-// res.status(201).json({newUserDetails: Users}) ;
-// }catch(err){
-//   console.log(err) ;
-// res.status(500).json({
-// error:err
-// })
-// } 
-// };
 
 exports.getUser=  async(req,res, next)=>{
 // const  Users=await Expense.findAll({ where : { userId: req.user.id}});
@@ -34,30 +30,29 @@ const  Users=await Expense.findAll();
 res.status(200).json({allUsers:Users}) 
  } ;
 
+ exports.deleteExpense = async (req, res, next) => {
+  const t = await sequelize.transaction();
+  try {
+    const id = req.params.id;
+    const user = await Expense.findOne({ where: { id: id } });
+    const response = await Expense.destroy({
+      where: { id: id },
+      transaction: t,
+    });
 
-
- 
- exports.deleteUser= async(req,res, next)=>{
-  try{
-  
-   const  uId = req.params.id ;
-
-Expense.destroy({where: { id:uId, userId: req.user.id }}).then((noofrows) => {
-  if(noofrows === 0){
-      return res.status(404).json({success: false, message: 'Expense doenst belong to the user'})
+    const totalExpenses = Number(req.user.totalExpenses) - Number(user.amount);
+    await req.user.update({ totalExpenses: totalExpenses }, { transaction: t });
+    if (response === 0) {
+      return res.status(401).json({
+        message: "Expense does not Belongs to User",
+        success: false,
+      });
+    }
+    await t.commit();
+    res
+      .status(200)
+      .json({ message: response, success: true, totalExpense: totalExpenses });
+  } catch (error) {
+    console.log("error:", error);
   }
-  return res.status(200).json({ success: true, message: "Deleted Successfuly"})
-}).catch(err => {
-  console.log(err);
-  return res.status(500).json({ success: true, message: "Failed"})
-})
-
-
-
-}catch(err){
-    console.log(err);
-    res.status(500).json(err)
-
-  }
-  
-  }
+}
