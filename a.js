6dd1e88422d83ec1635221abe53e51fc0dee4039
amanavinash <avinashverma13311@gmail.com>
2023-05-18 17:09:13 +0530

    gdfj

--- a.js
+++ a.js
@@ -1,11 +1,8 @@
 const express = require('express')
-const app = express()
-  
+const app = express() 
 app.get("/singup", (req, res) => {
     res.send("Hello");
-});
-  
-app.listen(3000, () => {
+});app.listen(3000, () => {
     console.log("listening on http://localhost:3000");
 })
 
