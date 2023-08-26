059e49f474d6ef101d3e2f1453518ecb7a143e9c
amanavinash <119783660+amanavinash@users.noreply.github.com>
2023-08-26 18:27:33 +0530

    user mango-db
    user mango db

--- app.js
+++ app.js
@@ -1,44 +1,56 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const morgan = require("morgan");
-const helmet = require("helmet");
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+
 const app = express();
-require("dotenv").config();
-app.use(cors());
-app.use(helmet());
-app.use(morgan());
-const sequelize = require("./util/database");
-console.log(sequelize);
-const UserRouter = require("./Routes/UserRoutes");
-const ExpenseRouter = require("./Routes/ExpenseRoute");
-const PurchaseRouter = require("./Routes/PurchaseRouter");
-const PremiumRouter = require("./Routes/PremiumUser");
-const ForgetPasswordRouter = require("./Routes/ForgotRoute");
-const User = require("./Model/User");
-const Expense = require("./Model/Expense");
-const Order = require("./Model/Purchase");
-const ForgetPassword = require("./Model/Forgot");
-const DownloadedFiles = require("./Model/Download");
-User.hasMany(Expense);
-Expense.belongsTo(User);
-User.hasMany(Order);
-Order.belongsTo(User);
-User.hasMany(ForgetPassword);
-ForgetPassword.belongsTo(User);
-User.hasMany(DownloadedFiles);
-DownloadedFiles.belongsTo(User);
-app.use(bodyParser.json({ extended: true }));
-app.use("/user", UserRouter);
-app.use("/expense", ExpenseRouter);
-app.use("/purchase", PurchaseRouter);
-app.use("/preminum", PremiumRouter);
- app.use("/password", ForgetPasswordRouter);
-sequelize
-  .sync()
-  .then((res) => {
-    app.listen(4000);
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findById('5bab316ce0a7c75f783cb8a8')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+mongoose
+  .connect(
+    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
+  )
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Max',
+          email: 'max@test.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
+    app.listen(3000);
   })
-  .catch((err) => {
+  .catch(err => {
     console.log(err);
   });
